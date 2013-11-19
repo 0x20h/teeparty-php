a75a8ce8aa0e5b5e04d12f41d722d63310f75fba
0x20h <kohj@informatik.uni-marburg.de>
2013-11-19 12:37:05 +0100

    wip, new interface

--- src/Teeparty/Queue.php
+++ src/Teeparty/Queue.php
@@ -1,9 +1,29 @@
 <?php
-
 namespace Teeparty;
 
+/**
+ * Interface for queue implementations.
+ */
 interface Queue {
+    /**
+     * Retrieve a new task from the queue.
+     *
+     * @param string[] $channels channels to pop from
+     * @param int $timeout timeout for listening for new items.
+     *
+     * @return Task Next task to be processed. null if no task is pending.
+     */
     public function pop(array $channels, $timeout = 0);
-    public function push(Message $message, $channel);
+
+    /**
+     * Put a new Task into the queue.
+     *
+     * @param Task $task Task to be processed
+     * @param string $channel put task into the given channel.
+     * 
+     * @return void
+     * @throws QueueException If the Task could not be pushed.
+     */
+    public function push(Task $task, $channel);
 }
 
